0ac82122077f1219d8450787d877438fa7aa26a8
Song <tansongsong@gmail.com>
2017-10-14 23:50:43 +0800

    Create python读入.py
    python读入数据方法

---
+++ template/python读入.py
@@ -0,0 +1,19 @@
+def parser():
+    while 1:
+        data = list(input().split(' '))
+        for number in data:
+            if len(number) > 0:
+                yield(number)   
+
+input_parser = parser()
+
+def get_word():
+    global input_parser
+    return next(input_parser)
+
+def get_number():
+    data = get_word()
+    try:
+        return int(data)
+    except ValueError:
+        return float(data)
